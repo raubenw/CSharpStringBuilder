869e27e0394747074b747143279a265bf116cd96
raubenw <raubenw@gmail.com>
2022-02-16 04:42:43 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace Builder
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int count = 10;
+            string[] sillyStringArray = { "dog", "cow", "cat", "horse" };
+
+            StringBuilder sb = new StringBuilder("This is a new initial string", 200);
+            Console.WriteLine($"The length of the string is {sb.Length} and the capacity of sb = {sb.Capacity}");
+
+            sb.Append("The quick brown fox");
+            sb.Append("Jumped over the lazy dog");
+            sb.AppendLine();
+
+            sb.AppendFormat("He jumped over {0} times", count);
+            sb.AppendLine();
+
+            sb.Append("He also jumped over ");
+            sb.AppendJoin(',', sillyStringArray);
+
+            sb.Replace("cow", "kangaroo");
+            sb.Insert(0, "Hier kom dit nou ");
+
+            Console.WriteLine($"Now the string length is {sb.Length} and the capacity is  {sb.Capacity}");
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
